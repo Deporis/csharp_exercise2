5531d16d60b125e403749447e522aa8a7f59b3a5
Alfred Inga <alfredingarios@gmail.com>
2020-08-19 01:45:52 +0200

    Fase2

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LletresRepetides
 {
     class Program
     {
+        static char [] myName;
         static void Main(string[] args)
         {
             Fase1();
@@ -12,7 +14,6 @@ namespace LletresRepetides
         {
             Console.WriteLine("Insert your name");
             string name = Console.ReadLine();
-            char [] myName;
             myName = name.ToCharArray(0, name.Length);
             foreach(char letter in myName)
             {
@@ -39,7 +40,20 @@ namespace LletresRepetides
             }
             else if(answer == "No" | answer =="no") Console.WriteLine($"\nOk then, your name is {name}");
             else Console.WriteLine("You did not enter a yes or no. The program ends here, try again");
-
+            
+            Fase2();
+        }
+        static void Fase2()
+        {
+            Console.WriteLine("\nHere are your name's characters whether is a vowel or a consonant:");
+            List<char> theName = new List<char>(myName);
+            foreach(char chara in theName)
+            {
+                string vowel = "aeiou";
+                string letter = chara.ToString();
+                if(vowel.Contains(letter)) Console.WriteLine(letter + " is VOWEL"); // Contains is like "in" in Python
+                else Console.WriteLine(letter + " is CONSONANT");
+            }
         }
     }
 }
